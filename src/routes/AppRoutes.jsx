dcfd501be0e9680121288ce831bc04fd56510a68
Krishna Gharti <krishnagharti688@gmail.com>
2026-07-23 14:00:25 +0545

    Fix deployment issue

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -13,11 +13,11 @@ import CompanyHistory from "../pages/About/CompanyHistory/CompanyHistory";
 import AwardsRecognition from "../pages/About/AwardsRecognition/AwardsRecognition";
 
 // Projects
-import AllProjects from "../pages/projects/AllProjects/AllProjects";
-import OngoingProjects from "../pages/projects/OngoingProjects/OngoingProjects";
-import CompletedProjects from "../pages/projects/CompletedProjects/CompletedProjects";
-import UpcomingProjects from "../pages/projects/UpcomingProjects/UpcomingProjects";
-import ProjectMap from "../pages/projects/ProjectMap/ProjectMap";
+import AllProjects from "../pages/Projects/AllProjects/AllProjects";
+import OngoingProjects from "../pages/Projects/OngoingProjects/OngoingProjects";
+import CompletedProjects from "../pages/Projects/CompletedProjects/CompletedProjects";
+import UpcomingProjects from "../pages/Projects/UpcomingProjects/UpcomingProjects";
+import ProjectMap from "../pages/Projects/ProjectMap/ProjectMap";
 
 export default function AppRoutes() {
   return (
